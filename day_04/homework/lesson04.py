545681ceb91f793b4e9114ad06534ef993355847
sandrobero <sandrobero211@gmail.com>
2026-05-05 12:51:04 +0400

    commit

---
+++ day_04/homework/lesson04.py
@@ -0,0 +1,50 @@
+# input-ნიშნავს ინფორმაციის შეტანას მიწოდებას მაგ: პითონის ინფუთ ფუნქცია, მიკროფონი, კლავიატურა, მაუსი.
+# output-ნიშნავს ინფორმაციის გამოტანას მაგ: ყურსასმენები, პითონის ტერმინალი, მონიტორი.
+# snake case- არის ისეთი წერის სტილი რომლის დროსაც სიტყვები ერთმანეთისაგან გამოიყოფა (_)-თი
+name=input("enter your name: ")
+print(type(name))
+
+
+# ინახება სტრინგი-ნიშნავს ტექსტურ მონაცემებს
+str1="dog"
+str2="cat"
+str3="mouse"
+str4="cow"
+str5="lion"
+
+# ინახება ინტეჯერი-ნიშნავს  მთელ რიცხვს
+int1=33
+int2=12
+int3=60
+int4=67
+int5=99
+
+# ინახება ფლოატი-ნიშნავს ათწილად რიცხვს
+float1=3.6
+float2=4.2
+float3=5.5
+float4=7.1
+float5=6.8
+
+
+name1="sandro"
+num1=46
+num2=8.3
+
+print(type(name1))
+print(type(num1))
+print(type(num2))
+
+word1=input("enter your word: ")
+
+word2=input("enter your word: ")
+
+print(word1 + word2)
+
+_name=input("your name: ")
+_surname=input("your surname: ")
+_age=input("your age: ")
+_height=input("your height: ")
+_weight=input("your weight: ")
+
+print("I am " + _name + ",my surname is " + _surname + ".my age is " + _age + ".my height is " + _height + "cm.my weight is " + _weight + "kg.")
