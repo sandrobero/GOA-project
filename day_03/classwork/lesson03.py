ecc3bcafb7525d3fbffaddb4b95955658c46ed95
sandrobero <sandrobero211@gmail.com>
2026-05-08 15:48:43 +0400

    commit

---
+++ day_03/classwork/lesson03.py
@@ -0,0 +1,14 @@
+print(10 + 13)
+print(55 - 60)
+print(33 * 2)
+print(40 / 5)
+print(30 // 5)
+print(27 % 4)
+
+name=("sandro")
+age=("15")
+color=("green")
+car=("subaru forester")
+city=("tbilisi")
+
+print("Hello, I am " + name + ". " + "I am " + age + "years old." + "I like " + color + car  +  "and I live in " + city)
---
+++ day_04/classwork/lesson04.py
@@ -0,0 +1,24 @@
+name=input("enter your name: ")
+print("your name is " + name)
+
+surname=input("enter your surname: ")
+print("your surname is " + surname)
+age=input("enter your age: ")
+print("your age is" + age)
+
+city=input("enter your city: ")
+print("your city is" + city)
+
+country=input("enter your country: ")
+print("your country is" + country)
+
+_name="sandro"
+_surname="beruashvili"
+_age="15"
+_city="tbilisi"
+_country="georgia"
+print("hello I am " + _name + " " + _surname + ". " + "I am " +  _age + " " + "years old " + "I live in " + _city + " " + "and I was born in " + _country)
+
+print(type("43"))
+print(type(5))
+print(type(10.32))
