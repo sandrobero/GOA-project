0e2c5ca074b2106ebe2f6775037ff7cb9658dd93
sandrobero <sandrobero211@gmail.com>
2026-05-13 14:53:32 +0400

    commit

---
+++ day_06/homework/lesson06.py
@@ -0,0 +1,68 @@
+print(55 > 10)
+print(17 > 1)
+print(3 > 18)
+print(1 > 90)
+print(7 > 40)
+print(57 < 64)
+print(21< 15)
+print(51 < 60)
+print(6 < 14)
+print(5 < 10)
+print(11 >= 10)
+print(4 >= 15)
+print(5 >= 32)
+print(2 >= 99)
+print(1 >= 4 )
+print(2<= 12)
+print(15 <= 19)
+print(11 <= 67)
+print(61 <= 56)
+print(18 <= 70)
+print(23 == 14)
+print(17 == 17)
+print(9 == 22)
+print(1 == 11)
+print(78 == 78)
+
+
+#or-ეს უჭერს მხარს True-ს ვიდრე False-ს როდესაც ორივე არის ერთ შედარებაში ყოველთვის გამოიტანს True-ს
+print(6 > 5 or 10 < 30) 
+print(6 == 5 or 40 < 420) 
+print(6 < 5 or 12 > 1210) 
+#and-ეს უჭერს მხარს False-ს  როდესაც ორივე არის შედარებაში ყოველთვის გამოიტანს False-ს
+print(4 >= 4333 and 6666 < 1111)
+print(4 <= 4 and 112 > 14523)
+print(412 == 55 and 12 < 541)
+
+#(>)-ეს ნიშნავს მეტია
+#(<)-ეს ნიშნავს ნაკლებია
+#(>=)-ეს ამოწმებს უდრის თუ არა ერთი მნიშვნელობა მეორეს იგივე რაც მეტობა.
+#(<=)-ეს  ამოწმებს უდრის თუ არა ერთი მნიშვნელობა მეორეს იგივე რაც ნაკლებობა.
+#(==)-მკაცრი ტოლობა / შედარება ის ამოწმებს მნიშვნელობების მონაცემთა ტიპს
+
+number=int(input("enter number: " ))
+
+number1=6
+
+print(number > number1)
+
+name=input("enter your name: ")
+
+name1="sandro"
+
+print(name == name1)
+
+age=int(input("enter your age: "))
+
+print(age > 18)
+
+
+
+
+
+
+
+
+
+
+
